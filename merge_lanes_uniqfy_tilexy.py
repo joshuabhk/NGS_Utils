2846bf6a355b454886da41277aad14a4af1937f3
Bong-Hyun  Kim <bong-hyun.kim@nih.gov>
2016-11-08 10:38:53 -0500

    merge_lanes_for_illumna

---
+++ merge_lanes_uniqfy_tilexy.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+
+import sys
+from sys import maxsize
+from random import randint
+from os.path import exists
+
+class FASTQ_lane_unifier :
+	def __init__( self, fn, outfn ) :
+		self.fn = fn
+		self.tilexys = set()
+		if exists(outfn) :
+			print( "File %s already exits!"%outfn, file=sys.stderr )
+			sys.exit(-1)
+		else :
+			outfp = open(outfn, 'w')
+
+		self.count = 0
+		self.run( outfp )
+
+	def run( self, outfp ) :
+		for i, line in enumerate( open(self.fn) ) :
+			if i%4 == 0 :
+				outfp.write(self.uniquify_header(line))
+			else :
+				outfp.write(line)
+				
+
+	def uniquify_header( self, fastq_header, lane_number='1' ) :
+		header1, header2 = fastq_header.split()
+		l = header1.split(":")
+		lane,tile,x,y = l[3], l[4], l[5],l[6]
+		newid = ' '.join(l[4:7])
+		if newid not in self.tilexys :
+			self.tilexys.add(newid)
+			l[3] = lane_number
+			header1 = ':'.join(l)
+			return '%s %s\n'%(header1, header2)
+			
+		oldid = newid
+		while newid in self.tilexys :
+			x, y = randint(1,maxsize), randint(1,maxsize)
+			newid = '%s %s %s'%(tile,x,y)
+		else :
+			self.tilexys.add(newid)
+
+		l[3] = lane_number	
+		l[5] = str(x)
+		l[6] = str(y)
+		header1 = ':'.join(l)
+		self.count += 1
+		print( self.count, oldid, "==>>", newid )
+
+		return '%s %s\n'%(header1, header2)
+
+
+if __name__=='__main__' :
+	try :
+		fq = sys.argv[1]
+		ofq = sys.argv[2]
+
+		FASTQ_lane_unifier( fq, ofq )
+	except :
+		print( "%s <Input> <Output>"%sys.argv[0] )
